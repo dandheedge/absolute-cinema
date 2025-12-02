c8dc663ff21f7655525a68c73b88864cbe7475a8
dandhee.damarrama <dandhee.damarrama@gmail.com>
2025-12-02 13:30:54 +0700

    fix: update default theme to 'dark' in Vuetify configuration

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,7 +1,7 @@
 /**
  * plugins/vuetify.ts
  *
- * Framework documentation: https://vuetifyjs.com`
+ * Framework documentation: https://vuetifyjs.com
  */
 
 // Composables
@@ -13,6 +13,19 @@ import 'vuetify/styles'
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
   theme: {
-    defaultTheme: 'system',
+    defaultTheme: 'dark',
+    themes: {
+      dark: {
+        dark: true,
+        colors: {
+          // deep-orange-darken-3
+          primary: '#D84315',
+          // yellow-darken-4
+          secondary: '#F57F17',
+          background: '#050507',
+          surface: '#121212',
+        },
+      },
+    },
   },
 })
